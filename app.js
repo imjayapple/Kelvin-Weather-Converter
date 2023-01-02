d7bd055b009100e5795b7d65f7b3c2a2d573eb7b
imjayapple <thisisfakeemail@gmai.com>
2023-01-01 20:03:37 -0800

    Successful conversion of Kevlin to Celsius, and vice versa

---
+++ app.js
@@ -0,0 +1,9 @@
+//Let's start off by converting Kelvin to Celsius
+//This value will be constant so save it as such
+
+const kevlin = 293;
+
+//Celsius is similar to Kevlin, the only difference is that Celsius is always 273 degrees less than Kelvin
+//Below is the conversion of Kelvin to Celsius
+
+const celsius = kelvin - 273;
