2e51a8e749f24b68c9b0451b4b64293e91d4537a
imjayapple <thisisfakeemail@gmai.com>
2023-01-01 21:14:15 -0800

    Added Fahrenheit conversion formula and fixed a typo in the kelvin variable declaration

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 //Let's start off by converting Kelvin to Celsius
 //This value will be constant so save it as such
 
-const kevlin = 293;
+const kelvin = 293;
 
 //Celsius is similar to Kevlin, the only difference is that Celsius is always 273 degrees less than Kelvin
 //Below is the conversion of Kelvin to Celsius
 
 const celsius = kelvin - 273;
+
+//Now we can use Celsius combined with a simple formula to convert our given temperature into Fahrenheit
+
+const fahrenheit = celsius * (9/5) + 32;
+
